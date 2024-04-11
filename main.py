6e1d831450a3aad8908c25c9dc75ef1ca516abc2
aakashv100 <vardhan.aakash1@gmail.com>
2024-04-11 13:15:07 +0400

    Update main.py to use Trainer class for training and testing the model

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 import random
-
 import numpy as np
 import torch
-import torch.optim as optim
 from torch.optim.lr_scheduler import OneCycleLR
-
 from models.model_utils import save_model
 from utils.transform import CIFAR10Dataset
 
@@ -64,54 +61,30 @@ def setup_cifar10(config):
     return train_data, test_data, train_loader, test_loader
 
 
-from torch.optim.lr_scheduler import OneCycleLR
-from models.model_utils import save_model
-from utils import test, train
 
-
-def main(config, model, train_loader, test_loader, optimizer, criterion):
+def main(config, model, trainer):
     """
-    Main function that trains and tests a model using the given configuration.
+    Main function that trains and tests the model.
 
     Args:
-        config (dict): A dictionary containing the configuration parameters.
+        config (dict): Configuration parameters for training and testing.
         model: The model to be trained and tested.
-        train_loader: The data loader for the training dataset.
-        test_loader: The data loader for the testing dataset.
+        trainer: The trainer object responsible for training the model.
+        train_loader: The data loader for training data.
         optimizer: The optimizer used for training the model.
-        criterion: The loss criterion used for training the model.
 
     Returns:
-        list: A list containing the learning rates used during training.
+        list: List of learning rates used during training.
     """
 
-    ocp_scheduler = OneCycleLR(
-        optimizer,
-        max_lr=config["max_lr"],
-        steps_per_epoch=len(train_loader),
-        epochs=config["epochs"],
-        pct_start=5 / config["epochs"],
-        div_factor=100,
-        three_phase=False,
-        final_div_factor=100,
-        anneal_strategy="linear",
-    )
     lr = []
     for epoch in range(1, config["epochs"] + 1):
         print("EPOCH:", epoch)
-        train(
-            model,
-            config["device"],
-            train_loader,
-            optimizer,
-            criterion,
-            ocp_scheduler=ocp_scheduler,
-            epoch=epoch,
-        )
-        test(model, config["device"], test_loader)
+        trainer.train(epoch)
+        trainer.test()
 
     # format name of model file according to config['norm']
-    model_file = "model_" + config["norm"] + ".pth"
+    model_file = "model_gradcam_" + config["norm"] + ".pth"
     save_model(model, model_file)
 
     return lr
